305ded13277bd2c3ac372c40e09f96456398ffa0
Warry <hei.warry@gmail.com>
2026-03-31 07:02:24 +0300

    feat: add Resource not found exception

---
+++ src/main/java/com/example/ingredientspring/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.ingredientspring.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
